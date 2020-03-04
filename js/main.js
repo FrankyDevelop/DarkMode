40ef02ad998b159ca7dc98553c46be678d0e9ee4
FrankyDevelop <getstartedteam@gmail.com>
2020-03-04 21:56:12 +0000

    Guardando configuracion en localstorage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () => {
-  document.body.classList.toggle('dark');/*toggle: si tiene una clase se la v a aquitar 
+  document.body.classList.toggle('dark');/*toggle: si tiene una clase se la va a quitar 
   y si no se la va a añadir*/ 
   btnSwitch.classList.toggle('active');
+
+  //Guardar configuración en localstorage
+  if(document.body.classList.contains('dark')){
+     localStorage.setItem('darkmode','true');
+  }else{
+     localStorage.setItem('darkmode','false');
+  }
+
+   //Evitar quitar el modo oscuro al recargar la pagina
+   if (localStorage.setItem('darkmode') ==='true') {
+       document.body.classList.add('dark');
+       btnSwitch.classList.add('active');
+   } else {
+       document.body.classList.remove('dark');
+       btnSwitch.classList.remove('active');
+   }
 });
